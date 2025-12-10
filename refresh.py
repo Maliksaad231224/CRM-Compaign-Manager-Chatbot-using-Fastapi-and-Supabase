2f1b628ee9ecfedf3e5079dfcd6d6816b4a80f1d
Malik Saad Ahmed <161501277+Maliksaad231224@users.noreply.github.com>
2025-12-10 14:32:24 +0500

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -4,8 +4,8 @@ import os
 
 SUPABASE_URL="https://ahvzujttyrpwqefxbhkf.supabase.co"
 SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodnp1anR0eXJwd3FlZnhiaGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODI4NzEsImV4cCI6MjA4MDg1ODg3MX0.g6tnAtt1kD1-YlohgjnHlg-lUyIpo9ltTOR92afdUrA"
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str =SUPABASE_URL
+key: str =SUPABASE_KEY
 supabase: Client = create_client(url, key)
 table_name = "term_deposit_campaigns"
 
